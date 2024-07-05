e6b866589161c5b944381bae0a317e852845aa9c
mamta-chaudhari-rndtechnosoft <mamta.chaudhari@rndtechnosoft.in>
2024-07-05 18:04:58 +0530

    Ui change in my ask and my give

--- app/src/main/java/com/rndtechnosoft/bni/Activity/MyMatchesActivity.kt
+++ app/src/main/java/com/rndtechnosoft/bni/Activity/MyMatchesActivity.kt
@@ -11,14 +11,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rndtechnosoft.bni.Adapter.MyMatchAdapter
+import com.rndtechnosoft.bni.ApiConfig.RetrofitInstance
+import com.rndtechnosoft.bni.Model.MatchedCompany
+import com.rndtechnosoft.bni.Model.MyAllMatchesResponseData
 import com.rndtechnosoft.bni.R
 import com.rndtechnosoft.bni.Util.SaveSharedPreference
 import com.rndtechnosoft.bni.ViewModel.MyMatchViewModel
 import com.rndtechnosoft.bni.databinding.ActivityMyMatchesBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyMatchesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyMatchesBinding
-    private lateinit var viewModel: MyMatchViewModel
+    //private lateinit var viewModel: MyMatchViewModel
     private lateinit var adapter: MyMatchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +35,16 @@ class MyMatchesActivity : AppCompatActivity() {
         Toast.makeText(this@MyMatchesActivity,"Hello",Toast.LENGTH_SHORT).show()
 
 
-        viewModel = ViewModelProvider(this@MyMatchesActivity)[MyMatchViewModel::class.java]
+        //viewModel = ViewModelProvider(this@MyMatchesActivity)[MyMatchViewModel::class.java]
 
         binding.rvMyMatch.layoutManager = LinearLayoutManager(this)
 
+        getMyAllMatch()
 
         val token: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getToken()
         val userId: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getUserId()
 
-        viewModel.setMyAllMatch(token!!, userId!!)
+        /*viewModel.setMyAllMatch(token!!, userId!!)
 
         viewModel.getMyAllMatch().observe(this@MyMatchesActivity, Observer {
             adapter = MyMatchAdapter(this@MyMatchesActivity, it)
@@ -47,7 +54,7 @@ class MyMatchesActivity : AppCompatActivity() {
         viewModel.getErrorData().observe(this@MyMatchesActivity, Observer {
             Toast.makeText(this@MyMatchesActivity, "Error: $it", Toast.LENGTH_SHORT).show()
             Log.d("Api Response", "Error: $it")
-        })
+        })*/
 
         //myAllMatchList()
 
@@ -59,7 +66,7 @@ class MyMatchesActivity : AppCompatActivity() {
         val token: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getToken()
         val userId: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getUserId()
 
-        viewModel.setMyAllMatch(token!!, userId!!)
+        /*viewModel.setMyAllMatch(token!!, userId!!)
 
         viewModel.getMyAllMatch().observe(this@MyMatchesActivity, Observer {
             adapter = MyMatchAdapter(this@MyMatchesActivity, it)
@@ -69,7 +76,37 @@ class MyMatchesActivity : AppCompatActivity() {
         viewModel.getErrorData().observe(this@MyMatchesActivity, Observer {
             Toast.makeText(this@MyMatchesActivity, "Error: $it", Toast.LENGTH_SHORT).show()
             Log.d("Api Response", "Error: $it")
-        })
+        })*/
 
     }
+
+    private fun getMyAllMatch(){
+
+        val token: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getToken()
+        val userId: String? = SaveSharedPreference.getInstance(this@MyMatchesActivity).getUserId()
+
+        RetrofitInstance.apiInterface.myAllMatches(token!!,userId).enqueue(object : Callback<MyAllMatchesResponseData?> {
+            override fun onResponse(
+                call: Call<MyAllMatchesResponseData?>,
+                response: Response<MyAllMatchesResponseData?>
+            ) {
+                    if(response.isSuccessful){
+                        Toast.makeText(this@MyMatchesActivity,"Success !!",Toast.LENGTH_SHORT).show()
+                        val responseData:MyAllMatchesResponseData = response.body()!!
+                        val list:MutableList<MatchedCompany> = responseData.matchedCompanies as MutableList
+                        adapter = MyMatchAdapter(this@MyMatchesActivity, list)
+                        binding.rvMyMatch.adapter = adapter
+
+                    }
+                else{
+                        Toast.makeText(this@MyMatchesActivity,"Response Error: ${response.code()}",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<MyAllMatchesResponseData?>, t: Throwable) {
+                Toast.makeText(this@MyMatchesActivity,"Error: ${t.localizedMessage}",Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
